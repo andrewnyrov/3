7d1d9a4d9314539a5a90e21948de2b2c9b8a9f59
andrewnyrov <andrewnyr@mail.ru>
2024-05-19 04:36:28 +0500

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,8 @@
+name = 'Name: Andrew'
+print(name)
+age = 53
+new_age = age + 1
+print('Age: ' + str(age))
+print('New age: ' + str(new_age))
+is_student = True
+print('Is student: ' + str(is_student))
